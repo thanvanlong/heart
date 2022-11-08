367e9b616a42033de8655fb2d1112c8a99b76a05
annh <annh@aladintech.co>
2022-11-08 21:59:57 +0700

    long

---
+++ src/main/java/com/example/akiu/AkiuApplication.java
@@ -0,0 +1,13 @@
+package com.example.akiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkiuApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/akiu/Controller.java
@@ -0,0 +1,13 @@
+package com.example.akiu;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
